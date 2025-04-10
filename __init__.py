193682809230a2f975fb2886da56670a40f64941
Miroshinoo <miiroshinoo@gmail.com>
2025-04-10 17:05:23 +0200

    Update __init__.py
    Comment to check actions/workflow

--- __init__.py
+++ __init__.py
@@ -22,3 +22,5 @@ def encryptage(valeur):
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
+
+# Commentaire pour vérification
